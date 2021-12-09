475e5a0a822535f7c202f165de596c227c921c58
lhalim6 <lhalim6@gatech.edu>
2021-12-09 00:06:59 -0500

    fully automated plotting script

--- results/plotConvergenceHistory.py
+++ results/plotConvergenceHistory.py
@@ -1,37 +1,68 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import niceplots
+import os
 
-number = str(28)
+dir_name = "/home/lhalim/git/optHist/results"
+direct = os.listdir(dir_name)
 
-lines = []
-with open('optHist' + number + '.txt') as f:
-    lines = f.readlines()
+for item in direct:
+    if item.endswith(".png"):
+        os.remove(os.path.join(dir_name, item))
 
-case = 'Aerothermal'
-objFunc = 'Temperature'
+jobs = [19,20,22,23,27,28,29,33,34,35]
+jobs = [19,20,22,23,27,28,29]
 
-iteration = 0
-iterations = []
-funcValues = []
-for index, line in enumerate(lines):
-    if line[0:15]=='Objective Value':
-        iteration += 1
-        iterations.append(iteration)
-        funcValues.append(float(line[20:]))
+for number in jobs:
+    lines = []
+    with open('optHist' + str(number) + '.txt') as f:
+        lines = f.readlines()
 
-print(iterations)
-print(funcValues)
+    vec_ATE = [19,20,22,23]
+    vec_AT = [27,28,33,34]
+    vec_KS = [19,22,27,29,33,35]
+    vec_SUB = [19,20,27,28,29]
 
-scalingFactor = 1 #0.010686
-for index, funcValue in enumerate(funcValues):
-    funcValues[index] = funcValues[index]*scalingFactor
+    if number in vec_ATE:
+        case = 'Aerothermoelastic'
+    elif number in vec_AT:
+        case = 'Aerothermal'
+    else:
+        case = 'Aeroelastic'
 
-niceplots.setRCParams()
+    if number in vec_KS:
+        objFunc = 'KSFailure'
+    else:
+        objFunc = 'Temperature'
 
-plt.plot(iterations, funcValues)
-plt.xlabel('Iterations')
-plt.ylabel(objFunc)
-plt.savefig(case + '_' + objFunc + number + '.png')
-#plt.show()
+    if number in vec_SUB:
+        regime = 'Subsonic'
+    else:
+        regime = 'Supersonic'
+
+    iteration = 0
+    iterations = []
+    funcValues = []
+    for index, line in enumerate(lines):
+        if line[0:15]=='Objective Value':
+            iteration += 1
+            iterations.append(iteration)
+            funcValues.append(float(line[20:]))
+
+    #print(iterations)
+    #print(funcValues)
+
+    scalingFactor = 1 #0.010686
+    for index, funcValue in enumerate(funcValues):
+        funcValues[index] = funcValues[index]*scalingFactor
+
+    niceplots.setRCParams()
+
+    plt.plot(iterations, funcValues)
+    plt.xlabel('Iterations')
+    plt.ylabel(objFunc)
+    plt.savefig(case + '_' + objFunc + '_' + regime + '.png')
+    plt.clf()
+    #plt.show()
 
 
